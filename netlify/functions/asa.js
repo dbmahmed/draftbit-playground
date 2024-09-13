c915273325a1e1a886b56b1d0aa64ce1c0c60d1e
db-maruf <maruf@draftbit.com>
2024-09-14 02:48:42 +0300

    add asa file

---
+++ netlify/functions/asa.js
@@ -0,0 +1,11 @@
+exports.handler = async function (event, context) {
+  return {
+    statusCode: 200,
+    statusDescription: "OK",
+    headers: { "content-type": { value: "application/json" } },
+    body: {
+      encoding: "text",
+      data: '{"applinks":{"details":[{"appIDs":["V2J3KK598N.com.draftbit.playground"],"components":[{"/":"/","comment":"Matches any URL with a path that starts with /test/."}]}]},"webcredentials":{"apps":["V2J3KK598N.com.draftbit.playground"]}}',
+    },
+  };
+};
