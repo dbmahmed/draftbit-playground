a84a4cc97b8511640942fc42ef0f2d0f569ccec1
db-maruf <maruf@draftbit.com>
2024-09-14 02:59:03 +0300

    update the json

--- netlify/functions/asa.js
+++ netlify/functions/asa.js
@@ -5,7 +5,23 @@ exports.handler = async function (event, context) {
     headers: { "content-type": { value: "application/json" } },
     body: {
       encoding: "text",
-      data: '{"applinks":{"details":[{"appIDs":["V2J3KK598N.com.draftbit.playground"],"components":[{"/":"/","comment":"Matches any URL with a path that starts with /test/."}]}]},"webcredentials":{"apps":["V2J3KK598N.com.draftbit.playground"]}}',
+      data: JSON.stringify({
+        applinks: {
+          details: [
+            {
+              appIDs: ["V2J3KK598N.com.draftbit.playground"],
+              components: [
+                {
+                  "/": "/",
+                  comment:
+                    "Matches any URL with a path that starts with /test/.",
+                },
+              ],
+            },
+          ],
+        },
+        webcredentials: { apps: ["V2J3KK598N.com.draftbit.playground"] },
+      }),
     },
   };
 };
