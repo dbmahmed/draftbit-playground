8f72a7172f41f97b0c9a0fc5f4e4982e396051e5
db-maruf <maruf@draftbit.com>
2024-09-14 03:11:09 +0300

    update config

--- netlify/functions/asa.js
+++ netlify/functions/asa.js
@@ -1,11 +1,10 @@
 exports.handler = async function (event, context) {
   return {
     statusCode: 200,
-    statusDescription: "OK",
-    headers: { "content-type": { value: "application/json" } },
-    body: {
-      encoding: "text",
-      data: "{\"applinks\":{\"details\":[{\"appIDs\":[\"V2J3KK598N.com.draftbit.playground\"],\"components\":[{\"/\":\"/test/*\",\"comment\":\"Matches any URL with a path that starts with /test/.\"}]}]},\"webcredentials\":{\"apps\":[\"V2J3KK598N.com.draftbit.playground\"]}}",
-    },
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			'Content-Type': 'application/json; charset=utf-8',
+		},
+    body: "{\"applinks\":{\"details\":[{\"appIDs\":[\"V2J3KK598N.com.draftbit.playground\"],\"components\":[{\"/\":\"/test/*\",\"comment\":\"Matches any URL with a path that starts with /test/.\"}]}]},\"webcredentials\":{\"apps\":[\"V2J3KK598N.com.draftbit.playground\"]}}",
   };
 };
