e34e741aede1a9bc27215b4f4b0a0529144f257b
dbmahmed <113385495+dbmahmed@users.noreply.github.com>
2024-10-09 09:51:19 +0300

    Update asa.js

--- netlify/functions/asa.js
+++ netlify/functions/asa.js
@@ -1,13 +1,35 @@
 exports.handler = async function (event, context) {
-console.log(event.path)
-    // Redirect to the Netlify function
-    return {
+  return {
     statusCode: 200,
-		headers: {
-			'Access-Control-Allow-Origin': '*',
-			'Content-Type': 'application/json; charset=utf-8',
-		},
-    body: `{\"activitycontinuation\":{\"apps\" : [\"V2J3KK598N.com.draftbit.playground\"]},\"applinks\":{\"details\":[{\"appIDs\":[\"V2J3KK598N.com.draftbit.playground\"],\"components\":[{\"/\":\"/test/*\",\"comment\":\"Matches any URL with a path that starts with /test/.\"}]}]},\"webcredentials\":{\"apps\":[\"V2J3KK598N.com.draftbit.playground\"]}}`,
-  }; // <TEAMID>.<bundleIdentifier>
-  
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+    body: JSON.stringify(
+
+      {
+        "applinks": {
+          "details": [{
+            "appIDs": [
+              "V2J3KK598N.com.draftbit.playground"
+            ],
+            "components": [{
+                "/": "/app/*",
+                "comment": "Matches all routes"
+              }
+          }]
+        },
+        "activitycontinuation": {
+          "apps": [
+            "V2J3KK598N.com.draftbit.playground"
+          ]
+        },
+        "webcredentials": {
+          "apps": [
+            "V2J3KK598N.com.draftbit.playground"
+          ]
+        }
+      }
+    )
+  };
 };
